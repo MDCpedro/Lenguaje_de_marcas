40f7548fc34b3295f41aca8023d54719c3abbad2
MDCpedro <pedromdc101@gmail.com>
2023-12-05 20:21:33 +0100

    Act  JavaScript

--- JavaScript Act3.1/Ex4.js
+++ JavaScript Act3.1/Ex4.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+const rl = readline.createInterface({ input, output });
 
-fs.readFile('./Ex4.js', 'utf-8', (error, datos) => {
-    if (error) {
-        console.log('error al leer', error)
-    } else {
-        console.log(datos)
-    }
+rl.question('Escribe la ruta del archivo: ', (rutaescrita) => {
+    ruta(rutaescrita);
 });
+
+function ruta(rutaescrita) {
+    fs.readFile(`${rutaescrita}`, 'utf-8', (error, datos) => {
+        if (error) {
+            console.log('Error al leer el archivo', error);
+        } else {
+            console.log(datos);
+        }
+        rl.close(); 
+    });
+}
