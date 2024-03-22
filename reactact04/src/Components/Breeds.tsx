886081531cb556de83b68e01328e91139ef6b906
MDCpedro <pedromdc101@gmail.com>
2024-03-22 20:22:18 +0100

    guardado

--- reactact04/src/Components/Breeds.tsx
+++ reactact04/src/Components/Breeds.tsx
@@ -1,13 +1,78 @@
 import React from "react";
 
+export type Breeds = Breed[]
+
+export interface Breed {
+  weight: Weight
+  id: string
+  name: string
+  cfa_url?: string
+  vetstreet_url: string
+  vcahospitals_url?: string
+  temperament: string
+  origin: string
+  country_codes: string
+  country_code: string
+  description: string
+  life_span: string
+  indoor: number
+  lap?: number
+  alt_names: string
+  adaptability: number
+  affection_level: number
+  child_friendly: number
+  dog_friendly: number
+  energy_level: number
+  grooming: number
+  health_issues: number
+  intelligence: number
+  shedding_level: number
+  social_needs: number
+  stranger_friendly: number
+  vocalisation: number
+  experimental: number
+  hairless: number
+  natural: number
+  rare: number
+  rex: number
+  suppressed_tail: number
+  short_legs: number
+  wikipedia_url: string
+  hypoallergenic: number
+  reference_image_id: string
+  image: Image
+}
+
+export interface Weight {
+  imperial: string
+  metric: string
+}
+
+export interface Image {
+  id: string
+  width: number
+  height: number
+  url: string
+}
+
+
+
 export default function Razas() {
-    const [razas, setRazas] = React.useState([] as Razas);
+    const [razas, setRazas] = React.useState([] as Breeds);
 
     React.useEffect(() => {
-        fetch("link api")
-        .then((response) => response.json())
-        .then((data: Razas) => {
-            setRazas(data);
+        fetch("https://api.thedogapi.com/v1/breeds", {
+          headers: {
+            "x-api-key": "123456"
+          },
         })
-    })
+        .then((response) => response.json())
+        .then((data: Breeds) => {
+            setRazas(data);
+        });
+    }, []);
+    return <>{razas.map((raza, i) => {
+      return <p key={i}>{raza.name}</p>
+    
+    })}</>
 }
